10b25fa3bb7df26c6438a68664c0123cee5ceb26
unknown <thalesalves758@gmail.com>
2021-08-26 11:17:26 -0300

    create Journal model with casts and fillable

---
+++ app/Models/Journal.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Journal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'localization',
+        'title',
+        'printing',
+        'idiom',
+        'creation_date',
+        'end_date'
+    ];
+
+    protected $casts = [
+        'creation_date' => 'datetime',
+        'end_date' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+}
