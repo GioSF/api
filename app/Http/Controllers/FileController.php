d80d30587a1fe93e81aad6d141177a1b7aa7727c
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-08-26 19:28:40 -0300

    #4  CardController-create-show-destroy-edit-update

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('file.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        File::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'filepath' => $request->filepath,
+            'hash_name' => $request->hash_name,
+        ]);
+        return redirect()->route('file.create');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $files = File::all();
+        return view('file.show', ['file' => $files]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $file = File::findOrFail($id);
+        return view('file.edit', ['file' => $file]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $file = File::findOrFail($id);
+        $file->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'filepath' => $request->filepath,
+            'hash_name' => $request->hash_name,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $file = File::findOrFail($id);
+        $file->delete();
+        return redirect()->route('file.create');
+    }
+}
