11200308660f5729669e7b7fafa4b59c9d4cbe46
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-08-25 22:51:58 -0300

    #1 CardController-create-show-destroy

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('card.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Card::create([
+            'subject' => $request->subject,
+            'date_issue' => $request->date_issue,
+            'duration_issue' => $request->duration_issue,
+            'abstract' => $request->abstract,
+            'comment' => $request->comment,
+            'issue' => $request->issue,
+        ]);
+        return redirect()->route('card.create');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cards = Card::all();
+        return $cards;
+        //return view('card.show', ['cards' => $cards]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $card = Card::findOrFail($id);
+        // return view('card.edit', ['card' => $card]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $card = Card::findOrFail($id);
+        $card->update([
+            'subject' => $request->subject,
+            'date_issue' => $request->date_issue,
+            'duration_issue' => $request->duration_issue,
+            'abstract' => $request->abstract,
+            'comment' => $request->comment,
+            'issue' => $request->issue,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $card = Card::findOrFail($id);
+        $card->delete();
+    }
+}
