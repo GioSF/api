8906a16b01ea186b428d8288b63da11b4444a576
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-08-27 01:12:07 -0300

    #6 SystemFunctionModel

---
+++ app/Models/SystemFunction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SystemFunction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function organizations(){
+        return $this->belongsToMany(Organization::class, 'organization_system_functions', 'id_system_functions', 'id_organization');
+    }
+}
