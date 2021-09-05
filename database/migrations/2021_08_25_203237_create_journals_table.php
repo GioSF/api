b5582ceb8e0db2f20365e658aa2f172329b9d709
Giovanni Stroppa <giosfdev@gmail.com>
2021-09-05 17:42:04 -0300

    Updates remaining files from Journals implementation

--- database/migrations/2021_08_25_203237_create_journals_table.php
+++ database/migrations/2021_08_25_203237_create_journals_table.php
@@ -6,32 +6,34 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateJournalsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('journals', function (Blueprint $table) {
-            $table->id();
-            $table->text('localization')->nullable();
-            $table->text('title')->nullable();
-            $table->text('printing')->nullable();
-            $table->text('idiom')->nullable();
-            $table->dateTime('creation_date')->nullable();
-            $table->dateTime('end_date')->nullable();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('journals', function (Blueprint $table) {
+			$table->id();
+			$table->text('title');
+			$table->text('initials')->nullable();
+			$table->text('format_type')->nullable();
+			$table->text('localization')->nullable();
+			$table->text('printing')->nullable();
+			$table->text('idiom')->nullable();
+			$table->dateTime('creation_date')->nullable();
+			$table->dateTime('end_date')->nullable();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('journals');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('journals');
+	}
 }
