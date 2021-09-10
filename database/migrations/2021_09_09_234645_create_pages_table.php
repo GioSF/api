25846b63e158b08c2c6940ce4e7186edde25a716
unknown <thalesalves758@gmail.com>
2021-09-09 21:27:09 -0300

    create Page migration

---
+++ database/migrations/2021_09_09_234645_create_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('page_number');
+            $table->char('filepath');
+            $table->text('content');
+            $table->unsignedBigInteger('issue_id');
+            $table->foreign('issue_id')->references('id')->on('issues')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
