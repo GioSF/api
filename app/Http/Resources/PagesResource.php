13dc39b2f97b29930b936e2f02c7fa4b334fa8c8
unknown <thalesalves758@gmail.com>
2021-09-09 21:29:04 -0300

    create Page resource

---
+++ app/Http/Resources/PagesResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PagesResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (string) $this->id,
+            'type' => 'Pages',
+            'attributes' => [
+                'page_number' => $this->page_number,
+                'filepath' => $this->filepath,
+                'content' => $this->content,
+                'issue_id' => (string) $this->issue_id,
+                'issue' => $this->issue,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at
+            ]
+        ];
+    }
+}
