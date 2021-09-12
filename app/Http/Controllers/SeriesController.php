5ef8b947159fd92a96c0f919eb12f9710096620c
unknown <thalesalves758@gmail.com>
2021-09-12 15:41:14 -0300

    #34 create Series controller

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Series;
+use Illuminate\Http\Request;
+use App\Http\Resources\SeriesResource;
+
+class SeriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return SeriesResource::collection(Series::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Series $series)
+    {
+        $series = $series->create($request->all());
+		
+		return new SeriesResource($series);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Series  $series
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Series $series)
+    {
+        return new SeriesResource($series);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Series  $series
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Series $series)
+    {
+        return new SeriesResource($series);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Series  $series
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Series $series)
+    {
+        $series->update($request->all());
+
+		return new SeriesResource($series);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Series  $series
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Series $series)
+    {
+        $series->delete();
+		return response(null, 204);
+    }
+}
