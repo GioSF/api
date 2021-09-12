f59dedb3a56e923057411e65018805f44cd28443
unknown <thalesalves758@gmail.com>
2021-09-12 15:39:55 -0300

    #34 create Series migration

---
+++ database/migrations/2021_09_12_182246_create_series_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('scope');
+            $table->text('content');
+            $table->text('note');
+            $table->text('volume');
+            $table->text('date_range');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
