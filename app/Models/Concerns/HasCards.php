d65ce21cdcbc9e3f1ebc7ab9f451f9791612c10a
unknown <thalesalves758@gmail.com>
2021-09-13 16:49:41 -0300

    #40 create HasCards trait

---
+++ app/Models/Concerns/HasCards.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Concerns;
+
+use App\Models\Card;
+
+trait HasCards
+{
+
+	public function cards(): \Illuminate\Database\Eloquent\Relations\MorphToMany
+	{
+		return $this->morphToMany(Card::class, 'cardable');
+	}
+}
