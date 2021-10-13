4d868dee99e14d96ba4aec7db3d84a112fdf6f7b
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-10-13 16:39:26 -0300

    #8 Update User

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function organizations(){
+        return $this->belongsToMany(Organization::class, 'organization_users', 'id_user', 'id_organization');
+    }
 }
