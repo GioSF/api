838634a370b2e890a536dc6a0d6855366372fbdd
Giovanni Stroppa <giosfdev@gmail.com>
2021-11-02 22:19:35 -0300

    45-Contribuible Applies tabs instead of spaces

--- app/Http/Controllers/ContribuitionController.php
+++ app/Http/Controllers/ContribuitionController.php
@@ -9,84 +9,84 @@ use Illuminate\Http\Request;
 
 class ContribuitionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return ContribuitionsResource::collection(Contribuition::all());
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		return ContribuitionsResource::collection(Contribuition::all());
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreUpdateContribuitionRequest $request, Contribuition $contribuition)
-    {
-        $contribuition = $contribuition->create($request->all());
-
-		return new ContribuitionsResource($contribuition);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Contribuition $contribuition)
-    {
-        return new ContribuitionsResource($contribuition);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Contribuition $contribuition)
-    {
-        return new ContribuitionsResource($contribuition);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(StoreUpdateContribuitionRequest $request, Contribuition $contribuition)
-    {
-        $contribuition->update($request->all());
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(StoreUpdateContribuitionRequest $request, Contribuition $contribuition)
+	{
+		$contribuition = $contribuition->create($request->all());
 
 		return new ContribuitionsResource($contribuition);
-    }
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Contribuition $contribuition)
-    {
-        $contribuition->delete();
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Contribuition $contribuition)
+	{
+		return new ContribuitionsResource($contribuition);
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Contribuition $contribuition)
+	{
+		return new ContribuitionsResource($contribuition);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(StoreUpdateContribuitionRequest $request, Contribuition $contribuition)
+	{
+		$contribuition->update($request->all());
+
+		return new ContribuitionsResource($contribuition);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Contribuition $contribuition)
+	{
+		$contribuition->delete();
 		return response(null, 204);
-    }
+	}
 }
