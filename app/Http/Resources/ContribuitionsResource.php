95f1faa356bffca64a4ee18dcb38a6c6ffa5fae5
Giovanni Stroppa <giosfdev@gmail.com>
2021-11-02 22:22:54 -0300

    45-Contribuible Applies tabs instead of spaces

--- app/Http/Resources/ContribuitionsResource.php
+++ app/Http/Resources/ContribuitionsResource.php
@@ -6,22 +6,22 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ContribuitionsResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return [
-            'id' => (string) $this->id,
-            'type' => 'Contribuitions',
-            'attributes' => [
-                'contribuition' => $this->contribuition,
-                'feedback_admin' => $this->feedback_admin,
-                'user_id' => $this->user_id
-            ]
-        ];
-    }
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+	 */
+	public function toArray($request)
+	{
+		return [
+			'id' => (string) $this->id,
+			'type' => 'Contribuitions',
+			'attributes' => [
+				'contribuition' => $this->contribuition,
+				'feedback_admin' => $this->feedback_admin,
+				'user_id' => $this->user_id
+			]
+		];
+	}
 }
