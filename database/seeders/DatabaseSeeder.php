b3dbdc231a54bcadf828bbc65d22c45742d66d15
Giovanni Stroppa <giosfdev@gmail.com>
2021-11-06 00:46:48 -0300

    8-OrganizationUsers Adds UserOrganizationSeeder on DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
 		\App\Models\User::factory(10)->create();
 		$this->call(\Database\Seeders\OrganizationSeeder::class);
 		$this->call(\Database\Seeders\JournalsSeeder::class);
+		$this->call(\Database\Seeders\UserOrganizationSeeder::class);
 	}
 }
