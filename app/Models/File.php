c176acb1badec5511031027c00cc3db419eee5b7
Giovanni Stroppa <giosfdev@gmail.com>
2021-11-06 01:53:42 -0300

    14-Fileables Adds build functions and eloquent relationships on File model

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\News;
+use App\Models\Organization;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class File extends Model
 {
@@ -13,7 +17,43 @@ class File extends Model
 		'title', 'description', 'filepath', 'hash_name',
 	];
 
-    public function fileable(){
-        return $this->morphTo();
-    }
+	static public function build(?string $title = null, ?string $description = null, ?string $filepath = null, ?string $hashName = null): self
+	{
+		$file = self::where('title', $title)
+			->where('description', $description)
+			->where('filepath', $filepath)
+			->first();
+
+		if (!$file)
+		{
+			$file = new File;
+			$file->title = $title;
+			$file->description = $description;
+			$file->filepath = $filepath;
+			$file->hash_name = Hash::make($filepath);
+		}
+
+		return $file;
+	}
+
+	public function fileable()
+	{
+		return $this->morphTo();
+	}
+
+	public function news()
+	{
+		return $this->morphedByMany(News::class, 'fileable');
+	}
+
+	public function organizations()
+	{
+		return $this->morphedByMany(Organization::class, 'fileable');
+	}
+
+	public function users()
+	{
+		return $this->morphedByMany(User::class, 'fileable');
+	}
+
 }
