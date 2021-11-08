640c9f67417c5d82863051d6bcafc3505436f978
Giovanni Stroppa <giosfdev@gmail.com>
2021-11-07 23:17:21 -0300

    6-systemFunction Changes spaces for tabs on system function migration

--- database/migrations/2021_10_12_190534_create_system_functions_table.php
+++ database/migrations/2021_10_12_190534_create_system_functions_table.php
@@ -6,29 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateSystemFunctionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('system_functions', function (Blueprint $table) {
-            $table->id();
-            $table->text('name');
-            $table->text('description');
-            $table->integer('parent_system_function');
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('system_functions', function (Blueprint $table) {
+			$table->id();
+			$table->text('name');
+			$table->text('description');
+			$table->integer('parent_system_function')->nullable();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('system_functions');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('system_functions');
+	}
 }
