0ee6b1abf59c79b719d9d453736fdd6a850084a4
Giovanni Stroppa <giosfdev@gmail.com>
2021-11-08 00:19:01 -0300

    21-migration-model-controller-role Implments Role build function

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,15 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    use HasFactory;
+	use HasFactory;
 
-    protected $fillable = ['slug', 'title', 'description'];
+	protected $fillable = ['slug', 'title', 'description'];
 
-    /*
-     * The users that belong to this role
-     */
-    public function users()
-    {
-        return $this->belongsToMany(User::class);
-    }
+	static public function build(?string $slug = null, ?string $title = null, ?string $description = null)
+	{
+		$role = self::where('slug', $slug)
+			->where('title', $title)
+			->first();
+		
+		if (!$role)
+		{
+			$role = new self;
+			$role->slug = $slug;
+			$role->title = $title;
+			$role->description = $description;
+		}
+
+		return $role;
+	}
+
+	/*
+	* The users that belong to this role
+	*/
+	public function users()
+	{
+		return $this->belongsToMany(User::class);
+	}
 }
