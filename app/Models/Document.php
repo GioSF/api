bf9692007365f32a9cbe64d3103e828e9f445153
Giovanni Stroppa <giosfdev@gmail.com>
2021-11-26 19:24:14 -0300

    58 Implements build function on Document model

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Concerns\HasCards;
+use App\Models\Organization;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,6 +16,25 @@ class Document extends Model
 		'file_path', 'content', 'organization_id'
 	];
 
+	static public function build(?string $content = null, ?string $file_path = null, Organization $organization = null): self
+	{
+		$document = self::where('content', $content)
+			->where('file_path', $file_path)
+			->where('organization', $organization->id)
+			->first();
+
+		if (!$document)
+		{
+			$document = new File;
+			$document->content = $content;
+			$document->file_path = $file_path;
+			$document->organization = $organization->id;
+		}
+
+		return $document;
+	}
+
+	
 	public function documentable()
 	{
 		return $this->morphTo();
