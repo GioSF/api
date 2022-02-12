6367967e2e49a1d60b7b9f7ed75999c1d4219575
Giovanni Stroppa <giosfdev@gmail.com>
2022-02-12 01:13:39 -0300

    71 Implements migrations for fields and form_fields - updates documents table

--- database/migrations/2022_01_23_041212_create_forms_table.php
+++ database/migrations/2022_01_23_041212_create_forms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDocumentsTable extends Migration
+class CreateFormsTable extends Migration
 {
 	/**
 	 * Run the migrations.
@@ -13,10 +13,9 @@ class CreateDocumentsTable extends Migration
 	 */
 	public function up()
 	{
-		Schema::create('documents', function (Blueprint $table) {
+		Schema::create('forms', function (Blueprint $table) {
 			$table->id();
-			$table->text('file_path');
-			$table->text('content');
+			$table->string('slug');
 			$table->unsignedBigInteger('organization_id');
 			$table->foreign('organization_id')->references('id')->on('organizations');
 			$table->timestamps();
@@ -30,6 +29,6 @@ class CreateDocumentsTable extends Migration
 	 */
 	public function down()
 	{
-		Schema::dropIfExists('documents');
+		Schema::dropIfExists('forms');
 	}
 }
